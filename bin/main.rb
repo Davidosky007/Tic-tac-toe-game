f697b240468a1953539fa87ddf0c2185ade302b6
David Bassey <davidosky1@gmail.com>
2020-11-19 12:35:48 +0100

    fixes the requested changes

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'pry'
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+# VARIABLES
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# METHODS
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts '-----------'
@@ -10,7 +14,27 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def draw_welcome
+def draw?(board)
+  board.all? { |i| i.is_a?(String) }
+end
+
+def win?(board)
+  combinations = [
+    [board[0], board[1], board[2]],
+    [board[3], board[4], board[5]],
+    [board[6], board[7], board[8]],
+    [board[0], board[3], board[6]],
+    [board[1], board[4], board[7]],
+    [board[2], board[5], board[8]],
+    [board[0], board[4], board[8]],
+    [board[2], board[4], board[6]]
+  ]
+  combinations.any? do |combo|
+    combo.all? { |i| i == 'X' } || combo.all? { |i| i == 'O' }
+  end
+end
+
+def welcome_title
   puts '************************************************************'
   puts '*                        TIC-TAC-TOE                       *'
   puts '************************************************************'
@@ -21,15 +45,81 @@ def draw_welcome
   puts 'Choose numbers from 1 to 9 to select desired cell'
   puts 'No duplicate numbers are allowed'
   puts '--------------------------------'
-  puts 'Player 1: What is your name?:'
-  player_one = gets.chomp
-  puts 'Player 2: What is your name?:'
-  player_two = gets.chomp
-  puts '************************************************************'
-  puts "#{player_one} gets to pick X , while #{player_two} gets to pick O"
-  puts '************************************************************'
-  puts 'Game Start!'
 end
 
-p draw_welcome
-p display_board(board)
+def validate_name(name)
+  loop do
+    break unless name.strip == ''
+
+    print 'Blank can not be used . please enter your name: '
+    name = gets.chomp
+  end
+  name
+end
+
+def player_name
+  puts 'please enter your name player 1:'
+  player1 = gets.chomp
+  player1 = validate_name(player1)
+  puts 'please enter your name player 2'
+  player2 = gets.chomp
+  player2 = validate_name(player2)
+  [player1, player2]
+end
+
+def validate_position(position, board)
+  loop do
+    break if (1..9).include?(position) && !board[position - 1].is_a?(String)
+
+    puts 'Please enter valid number from 1 to 9' unless (1..9).include?(position)
+    puts "it's already taken , Please choose another position: "
+    position = gets.chomp.to_i if board[position - 1].is_a?(String)
+  end
+  position
+end
+
+# START GAME INTRO
+welcome_title
+
+puts "enter any key to start Tic Tac Toe game, to quit game enter 'q"
+answer = gets.chomp.downcase
+abort if answer == 'q'
+
+player1, player2 = player_name
+
+puts "#{player1} will be using 'X', while #{player2} will be using 'O'"
+
+# DISPLAY THE LATEST BOARD TO PLAYER
+display_board(board)
+active_player = player1
+
+loop do
+  puts "#{active_player}: which poistion would you like to take?"
+
+  position = gets.chomp.to_i
+  position = validate_position(position, board)
+
+  # UPDATE THE BOARD WITH CURRENT PLAYER SYMBOL BASED ON PLAYER'S INPUT
+  board[position - 1] = active_player == player1 ? 'X' : 'O'
+
+  # BREAK REPEAT IF
+  if win?(board)
+    display_board(board)
+    puts 'Congratulations!'
+    puts "#{active_player} is the winner!"
+    break
+  elsif draw?(board)
+    display_board(board)
+    puts 'the game is a tie, try again'
+    break
+  end
+
+  # DISPLAY UPDATED BOARD
+  display_board(board)
+  # SWITCH CURRENT PLAYER
+  active_player = active_player == player1 ? player2 : player1
+end
+
+puts "if you wish to continue playing the game, enter any key. to quit the game press 'q"
+answer = gets.chomp.downcase
+abort if answer == 'q'
