ee785b1d0c598dd668c92beae29fd61f9cb123b9
David Bassey <davidosky1@gmail.com>
2020-11-28 16:04:42 +0100

    fixes requested changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Game
   def display_board(board)
     puts ''
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -49,15 +49,24 @@ describe Game do
       expect(new_game.validate_name('David')).to eql('David')
     end
   end
+
   describe '#validate_position' do
     it 'checks if the position taken is valid' do
       expect(new_game.validate_position(3, board.board)).to eql(3)
+    end
+
+    it 'returns false if the position taken is not valid' do
+      expect(new_game.validate_position(11, board.board)).to_not eql(true)
     end
   end
 
   describe '#game_status' do
     it 'returns false if the game is neither a win nor a draw ' do
       expect(new_game.game_status?(board, active_player)).to eql(false)
+    end
+
+    it 'returns true if the game is neither a win nor a draw ' do
+      expect(new_game.game_status?(board, active_player)).to_not eql(true)
     end
   end
 end
